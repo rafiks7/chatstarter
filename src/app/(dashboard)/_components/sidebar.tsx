de26f4b3a4a81134c861b8302aafd5caeb198aa6
Rafik Shukralla <rafik.shukralla@student.csulb.edu>
2024-10-27 17:27:36 -0700

    Add messages backend and UI

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -23,9 +23,14 @@ import Link from "next/link";
 import { PlusIcon, User2Icon } from "lucide-react";
 import { api } from "../../../../convex/_generated/api";
 import { SignOutButton } from "@clerk/nextjs";
+import { NewDirectMessage } from "./new-direct-message";
+import { usePathname } from "next/navigation";
+
 
 export function DashboardSidebar() {
     const user = useQuery(api.functions.user.get);
+    const directMessages = useQuery(api.functions.dm.list);
+    const pathname = usePathname();
   
     if (!user) {
       return null;
@@ -36,8 +41,8 @@ export function DashboardSidebar() {
           <SidebarGroup>
             <SidebarGroupContent>
               <SidebarMenu>
-                <SidebarMenuButton asChild>
-                  <Link href="/friends">
+                <SidebarMenuButton asChild isActive={pathname === "/"}>
+                  <Link href="/">
                     <User2Icon />
                     Friends
                   </Link>
@@ -46,10 +51,26 @@ export function DashboardSidebar() {
             </SidebarGroupContent>
             <SidebarGroup>
               <SidebarGroupLabel>Direct Messages</SidebarGroupLabel>
-              <SidebarGroupAction>
-                <PlusIcon />
-                <span className="sr-only">New Direct Message</span>
-              </SidebarGroupAction>
+              <NewDirectMessage />
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {directMessages?.map((directMessage) => (
+                    <SidebarMenuItem key={directMessage._id}>
+                      <SidebarMenuButton asChild isActive={pathname === `/dms/${directMessage._id}`}>
+                        <Link href={`/dms/${directMessage._id}`}>
+                          <Avatar className="size-6">
+                              <AvatarImage src={directMessage.user.image} />
+                              <AvatarFallback>
+                                    {directMessage.user.username[0]}
+                              </AvatarFallback>
+                          </Avatar>
+                          <p className="font-medium">{directMessage.user.username}</p>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
             </SidebarGroup>
           </SidebarGroup>
         </SidebarContent>
---
+++ src/app/(dashboard)/dms/[id]/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SendIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { api } from "../../../../../convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+import { use, useState } from "react";
+import { Doc, Id } from "../../../../../convex/_generated/dataModel";
+import { FunctionReturnType } from "convex/server";
+import { toast } from "sonner";
+
+export default function MessagePage({
+  params,
+}: {
+  params: Promise<{ id: Id<"directMessages"> }>;
+}) {
+  const { id } = use(params);
+  const directMessage = useQuery(api.functions.dm.get, { id });
+
+  const messages = useQuery(api.functions.message.list, { directMessage: id });
+
+  if (!directMessage) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col flex-1 divide-y max-h-screen">
+      <header className="flex items-center gap-2 p-4">
+        <Avatar className="size-8 border">
+          <AvatarImage src={directMessage.user.image} />
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+        <h1 className="font-semibold">{directMessage.user.username}</h1>
+      </header>
+      <ScrollArea className="h-full py-4">
+        {messages?.map((message) => (
+          <MessageItem key={message._id} message={message} />
+        ))}
+      </ScrollArea>
+      <MessageInput directMessage={id} />
+    </div>
+  );
+}
+
+type Message = FunctionReturnType<typeof api.functions.message.list>[number];
+
+function MessageItem({ message }: { message: Message }) {
+  const user = useQuery(api.functions.user.get);
+
+  return (
+    <div className="flex items-center gap-2 px-4 py-2">
+      <Avatar className="size-8 border">
+        {message.sender && (
+          <>
+            <AvatarImage src={message.sender.image} />
+            <AvatarFallback></AvatarFallback>
+          </>
+        )}
+      </Avatar>
+      <div className="flex flex-col mr-auto">
+        <p className="text-xs text-muted-foreground">
+          {message.sender?.username ?? "Deleted User"}
+        </p>
+        <p className="text-sm ">{message?.content}</p>
+      </div>
+      <MessageActions message={message} />
+    </div>
+  );
+}
+
+function MessageActions({ message }: { message: Message }) {
+  const user = useQuery(api.functions.user.get);
+  const removeMessage = useMutation(api.functions.message.remove);
+
+  if (!user || message.sender?._id !== user._id) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <MoreVerticalIcon className="size-4 text-muted-foreground" />
+        <span className="sr-only">Message Actions</span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          className="text-destructive"
+          onClick={() => removeMessage({ id: message._id })}
+        >
+          <TrashIcon />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function MessageInput({
+  directMessage,
+}: {
+  directMessage: Id<"directMessages">;
+}) {
+  const [content, setContent] = useState("");
+
+  const sendMessage = useMutation(api.functions.message.create);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await sendMessage({ content, directMessage });
+      setContent("");
+    } catch (error) {
+      toast.error("Failed to send message", {
+        description: error instanceof Error ? error.message : "Unknown error",
+      });
+    }
+  };
+
+  return (
+    <form className="flex items-center p-4 gap-2" onSubmit={handleSubmit}>
+      <Input
+        placeholder="Message"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <Button size="icon">
+        <SendIcon />
+        <span className="sr-only">Send</span>
+      </Button>
+    </form>
+  );
+}
