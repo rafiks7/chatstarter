dd22a14276e32f088e9085ed8afdc010ba672333
Rafik Shukralla <rafik.shukralla@student.csulb.edu>
2024-11-03 20:34:06 -0800

    change next.config to js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: new URL(process.env.NEXT_PUBLIC_CONVEX_URL).hostname,
+      },
+    ],
+  }
+};
+
+module.exports = nextConfig;
